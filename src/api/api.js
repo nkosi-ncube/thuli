0441fa2e815b58a410a4c28f68dfaedf0c37660b
Nkosilomusa Ncube <100485709+nkosi-ncube@users.noreply.github.com>
2025-02-08 15:56:09 +0200

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,31 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000"; // Backend URL
+const API_URL = "https://kathulis-backend.vercel.app"; // Backend URL
 
+// Frontend login function
+export const login = async (data) => {
+  try {
+    console.log(data);
+    const response = await axios.post(`${API_URL}/login`, {
+      name: data.name,
+      password: data.password,
+      role: data.role,
+    }, {
+      headers: {
+        "Content-Type": "application/json", // Ensure Content-Type is set to application/json
+      }
+    });
+    return response.data; // { status, name, role }
+  } catch (error) {
+    console.error("Login failed:", error.response?.data || error.message);
+    return { success: false, message: error.response?.data?.detail || error.message };
+  }
+};
 // Fetch all customers
 export const getCustomers = async () => {
   try {
     const response = await axios.get(`${API_URL}/customers/`);
+    console.log("api data: ",response.data);
     return response.data;
   } catch (error) {
     console.error("Error fetching customers:", error);
@@ -13,13 +33,13 @@ export const getCustomers = async () => {
   }
 };
 
+
 // Create a new customer
 export const createCustomer = async (customer) => {
   try {
-    console.log("Customer:" ,customer)
     const response = await axios.post(`${API_URL}/customers/`, {
       name: customer.name,
-      balance: customer.balance,
+      balance: customer.balance,      
       phone_number: customer.phone_number,
     });
     return response.data;
@@ -31,13 +51,13 @@ export const createCustomer = async (customer) => {
 // Update an existing customer
 export const updateCustomer = async (id, customer) => {
   try {
-    console.log("Inside API:",{"id":id,"name":customer.name,"money":customer.balance,"phone":customer.phone_number});
+    console.log("Updating customer with ID:", id);
     const response = await axios.put(`${API_URL}/customers/${id}`, {
       name: customer.name,
       balance: customer.balance,
       phone_number: customer.phone_number,
     });
-    console.log("Updating customer: ", response.data);
+    console.log("Response data:", response.data);  // Debugging the response
     return response.data;
   } catch (error) {
     console.error("Error updating customer:", error);
@@ -54,15 +74,3 @@ export const deleteCustomer = async (id) => {
   }
 };
 
-
-// Login function
-export const login = async (loginData) => {
-  try {
-    console.log("Login Data: ", loginData);
-    const response = await axios.post(`${API_URL}/login`, loginData);
-    return response.data; // Returns a success message or any other data from the backend
-  } catch (error) {
-    console.error("Error logging in:", error);
-    return { message: "Login failed", error: error.response?.data?.detail || error.message };
-  }
-};
